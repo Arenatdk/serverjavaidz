f0bd763098818d82189aa14364bcf39ccab010a5
arenatdk <arenatdk@gmail.com>
2016-11-29 14:33:47 +0200

    Hibernate go fuck MySQL connect

--- src/main/java/myidz/controller/MainController.java
+++ src/main/java/myidz/controller/MainController.java
@@ -1,10 +1,15 @@
 package myidz.controller;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 /**
  * Created by denis on 28.11.16.
@@ -20,20 +25,47 @@ public class MainController {
     @FXML
     AnchorPane  loginPanel;
     @FXML
+    TableView tabls;
+    @FXML
+    TableColumn IDcol;
+     @FXML
     AnchorPane  contentPanel;
     @FXML
     public void initialize(){
         contentPanel.setVisible(false);
         loginPanel.setVisible(true);
+        LoginText.setText("denis");
+        PassText.setText("pass");
 
     }
 
     public void btlloginEvent(){
-        //LoginText.setText("root");
-        if(LoginText.getText().equals("root") && PassText.getText().equals("pass")) {
-            contentPanel.setVisible(true);
-            loginPanel.setVisible(false);
+        LoginText.setText("denis");
+        PassText.setText("pass");
+        Connection connecttion;
+
+
+        try{
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connecttion = DriverManager.getConnection("jdbc:mysql://localhost:3306/myticket?autoReconnect=true&useSSL=false",LoginText.getText(), PassText.getText());
+            if (!connecttion.isClosed()){
+                System.out.print("Все ок");
+
+                contentPanel.setVisible(true);
+                loginPanel.setVisible(false);
+            }
         }
-        System.out.print(LoginText.getText());
+        catch (SQLException e)
+        {
+            btnlogin.setText("Не удалось");
+            System.out.print("Не удаломь загрузить драйвер");
+        }
+
+    }
+
+    public void ttt(){
+        //tabls.setItems();
+
     }
 }
